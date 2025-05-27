b99734b00052f4c9aaabd8ad8e0f522f242810b9
mikosavolainen <miko.markus2112@gmail.com>
2025-05-28 01:06:41 +0300

    warehouse delete update

--- warehouse.sql
+++ warehouse.sql
@@ -3,8 +3,19 @@
 -- Palvelinversio:               10.10.2-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Versio:              11.3.0.6295
+-- Skripti:                      Poistaa koko warehouse-tietokannan ja luo sen uudelleen
 -- --------------------------------------------------------
 
+-- 1. Poista vanha tietokanta (jos se on olemassa)
+DROP DATABASE IF EXISTS `warehouse`;
+
+-- 2. Luo uusi tyhjä tietokanta
+CREATE DATABASE `warehouse` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+
+-- 3. Valitse käytettäväksi tietokanta
+USE `warehouse`;
+
+-- 4. Laitetaan väliaikaisesti pois tietyt asetukset turvallisuutta varten
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -12,12 +23,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Dumping database structure for warehouse
-CREATE DATABASE IF NOT EXISTS `warehouse` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
-USE `warehouse`;
-
--- Dumping structure for taulu warehouse.products
+-- 5. Luodaan products-taulu
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -29,19 +35,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table warehouse.products: ~7 rows (suunnilleen)
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `category`, `desc`, `vanha`) VALUES
-	(1, 'FT240-43', 9.75, 68, 'ferrite', NULL, 0),
-	(2, 'FT240-61', 14.00, 50, 'ferrite', NULL, 0),
-	(3, 'rg-214', 1.59, 498, 'kaapeli', NULL, 0),
-	(4, 'FT-2423', 12.00, 799, 'ferrite', NULL, 0),
-	(7, 'rg-58', 1.00, 400, 'kaapeli', NULL, 0),
-	(8, 'FT-990', 399.00, 1, 'vanhat', 'Tästä toimiva ja hyvä Yaesu hyvään hintaan 100W ja sisäänrakennettulla virtalähteellä', 1),
-	(9, 'RTL-SDR-HF', 22.00, 2, 'Kaapeli', '', 0);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-
--- Dumping structure for taulu warehouse.purchases
+-- 6. Luodaan purchases-taulu
 CREATE TABLE IF NOT EXISTS `purchases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `callsign` varchar(255) NOT NULL,
@@ -52,16 +46,25 @@ CREATE TABLE IF NOT EXISTS `purchases` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table warehouse.purchases: ~4 rows (suunnilleen)
-/*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
-INSERT INTO `purchases` (`id`, `callsign`, `name`, `total_amount`, `date`, `items`) VALUES
-	(3, 'adad', 'sdas', 19.50, '2025-01-17T22.15.28Z', NULL),
-	(4, '2easd3q2', 'AaaaAaA', 227.43, '2025-01-17T22.15.51Z', NULL),
-	(5, 'asdada', 'asdasd', 19.50, '2025-01-17T22.31.43Z', NULL),
-	(6, 'asdada', 'asdasd', 29.25, '2025-01-17T22.31.49Z', NULL),
-	(7, 'oh3cyt', 'Miko', 19.50, '2025-01-17T22.35.36Z', NULL);
-/*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
+-- 7. Lisätään tuotteet products-tauluun
+INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `category`, `desc`, `vanha`) VALUES
+    (1, 'FT240-43', 9.75, 68, 'ferrite', NULL, 0),
+    (2, 'FT240-61', 14.00, 50, 'ferrite', NULL, 0),
+    (3, 'rg-214', 1.59, 498, 'kaapeli', NULL, 0),
+    (4, 'FT-2423', 12.00, 799, 'ferrite', NULL, 0),
+    (7, 'rg-58', 1.00, 400, 'kaapeli', NULL, 0),
+    (8, 'FT-990', 399.00, 1, 'vanhat', 'Tästä toimiva ja hyvä Yaesu hyvään hintaan 100W ja sisäänrakennettulla virtalähteellä', 1),
+    (9, 'RTL-SDR-HF', 22.00, 2, 'Kaapeli', '', 0);
 
+-- 8. Lisätään ostot purchases-tauluun
+INSERT INTO `purchases` (`id`, `callsign`, `name`, `total_amount`, `date`, `items`) VALUES
+    (3, 'adad', 'sdas', 19.50, '2025-01-17T22.15.28Z', NULL),
+    (4, '2easd3q2', 'AaaaAaA', 227.43, '2025-01-17T22.15.51Z', NULL),
+    (5, 'asdada', 'asdasd', 19.50, '2025-01-17T22.31.43Z', NULL),
+    (6, 'asdada', 'asdasd', 29.25, '2025-01-17T22.31.49Z', NULL),
+    (7, 'oh3cyt', 'Miko', 19.50, '2025-01-17T22.35.36Z', NULL);
+
+-- 9. Palautetaan alkuperäiset asetukset
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
